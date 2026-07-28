d3f41ecbf046a6073fb5fff2adb3283364cfea4e
Yurii Mironov <yuriymironov96@gmail.com>
2026-07-28 22:33:06 +0300

    feat: lesson 15 - a button-controlled led project

--- README.md
+++ README.md
@@ -6,3 +6,23 @@ A bunch of learning projects using ESP32-S3-N16R8 developemt board.
 - [Schematic](https://99tech.com.au/mx-m/esp32/esp32-s3-yd_schematics.pdf);
 - [ESP32-S3-WROOM-1 Datasheet](https://documentation.espressif.com/esp32-s3-wroom-1_wroom-1u_datasheet_en.pdf);
 - [Pinout](https://lastminuteengineers.com/wp-content/uploads/iot/ESP32-S3-DevKitC-Pinout.png);
+
+## Lesson 15
+
+### Task
+- Connect two LEDs to any GPIO outputs of the ESP32.
+- Connect an external button to the GPIO.
+- Use the BOOT button (GPIO0) as a second button without additional connections.
+- Implement two LED blinking modes:
+  - mode activated by an external button;
+  - mode activated by the BOOT button.
+- When pressing the external button, switch the LEDs to a faster blinking mode.
+- When pressing the BOOT button, switch the LEDs to a slower blinking mode.
+- Implement blinking via delay() (I have used millis()-based solution to avoid blocks and stutters).
+- Eliminate the rattling of the button contacts (delay after the button is pressed).*
+2. Additional (optional):
+- Add a third mode that is activated by a long press of the button or by pressing both buttons simultaneously (I have implemented a gradual accelaration mode).
+- Instead of two modes, implement cyclic switching between three or more speeds.
+- Add output to Serial Monitor with a message about the selected mode.
+
+![ESP32 LED controller demo](demo.gif)
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,161 @@
 #include <Arduino.h>
 
-#define LED_PIN 38
+// pins
+constexpr uint8_t SLOWER_BUTTON_PIN = 0;
+constexpr uint8_t FASTER_BUTTON_PIN = 36;
+constexpr uint8_t RED_LED_PIN = 38;
+constexpr uint8_t BLUE_LED_PIN = 45;
 
-void setup() {
-    pinMode(LED_PIN, OUTPUT);
+// blink intervals
+constexpr unsigned long SLOW_BLINK_INTERVAL_MS = 2000;
+constexpr unsigned long FAST_BLINK_INTERVAL_MS = 250;
+constexpr unsigned long MIN_ACCELERATION_INTERVAL_MS = 100;
+
+// rate of acceleration for the "special" mode
+float ACCELERATION_RATIO = 0.8;
+
+// button debounce
+constexpr unsigned long DEBOUNCE_DELAY_MS = 50;
+
+// button levels depend on the circuit wiring.
+constexpr bool SLOWER_BUTTON_PRESSED_LEVEL = LOW;
+constexpr bool FASTER_BUTTON_PRESSED_LEVEL = HIGH;
+
+unsigned long blinkIntervalMs = SLOW_BLINK_INTERVAL_MS;
+unsigned long previousBlinkTimeMs = 0;
+
+bool redLedIsOn = true;
+bool isAccelerating = false;
+
+bool previousSlowerButtonState = !SLOWER_BUTTON_PRESSED_LEVEL;
+bool previousFasterButtonState = !FASTER_BUTTON_PRESSED_LEVEL;
+
+void updateBlinking();
+void updateButtons();
+void setLedStates();
+void enableSlowMode();
+void enableFastMode();
+void enableAccelerationMode();
+
+void setup()
+{
+    Serial.begin(9600);
+
+    pinMode(SLOWER_BUTTON_PIN, INPUT);
+    pinMode(FASTER_BUTTON_PIN, INPUT);
+
+    pinMode(RED_LED_PIN, OUTPUT);
+    pinMode(BLUE_LED_PIN, OUTPUT);
+
+    setLedStates();
+
+    Serial.println("ESP32 application started");
 }
 
 void loop()
 {
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-    delay(500);
+    updateButtons();
+    updateBlinking();
+}
+
+void updateBlinking()
+{
+    const unsigned long currentTimeMs = millis();
+
+    if (currentTimeMs - previousBlinkTimeMs < blinkIntervalMs)
+    {
+        return;
+    }
+
+    previousBlinkTimeMs = currentTimeMs;
+
+    redLedIsOn = !redLedIsOn;
+    setLedStates();
+
+    if (!isAccelerating)
+    {
+        return;
+    }
+
+    blinkIntervalMs =
+        blinkIntervalMs * ACCELERATION_RATIO;
+
+    if (blinkIntervalMs <= MIN_ACCELERATION_INTERVAL_MS)
+    {
+        blinkIntervalMs = SLOW_BLINK_INTERVAL_MS;
+    }
+}
+
+void updateButtons()
+{
+    const bool currentSlowerButtonState =
+        digitalRead(SLOWER_BUTTON_PIN);
+
+    const bool currentFasterButtonState =
+        digitalRead(FASTER_BUTTON_PIN);
+
+    const bool slowerButtonIsHeld =
+        currentSlowerButtonState == SLOWER_BUTTON_PRESSED_LEVEL;
+
+    const bool fasterButtonIsHeld =
+        currentFasterButtonState == FASTER_BUTTON_PRESSED_LEVEL;
+
+    const bool slowerButtonWasPressed =
+        slowerButtonIsHeld &&
+        previousSlowerButtonState != SLOWER_BUTTON_PRESSED_LEVEL;
+
+    const bool fasterButtonWasPressed =
+        fasterButtonIsHeld &&
+        previousFasterButtonState != FASTER_BUTTON_PRESSED_LEVEL;
+
+    if (slowerButtonIsHeld && fasterButtonIsHeld)
+    {
+        if (!isAccelerating)
+        {
+            enableAccelerationMode();
+        }
+    }
+    else if (slowerButtonWasPressed)
+    {
+        enableSlowMode();
+    }
+    else if (fasterButtonWasPressed)
+    {
+        enableFastMode();
+    }
+
+    previousSlowerButtonState = currentSlowerButtonState;
+    previousFasterButtonState = currentFasterButtonState;
+
+    delay(DEBOUNCE_DELAY_MS);
+}
+
+void setLedStates()
+{
+    digitalWrite(RED_LED_PIN, redLedIsOn ? HIGH : LOW);
+    digitalWrite(BLUE_LED_PIN, redLedIsOn ? LOW : HIGH);
+}
+
+void enableSlowMode()
+{
+    blinkIntervalMs = SLOW_BLINK_INTERVAL_MS;
+    isAccelerating = false;
+
+    Serial.println("Slow mode enabled");
+}
+
+void enableFastMode()
+{
+    blinkIntervalMs = FAST_BLINK_INTERVAL_MS;
+    isAccelerating = false;
+
+    Serial.println("Fast mode enabled");
+}
+
+void enableAccelerationMode()
+{
+    blinkIntervalMs = SLOW_BLINK_INTERVAL_MS;
+    isAccelerating = true;
+
+    Serial.println("Acceleration mode enabled");
 }
